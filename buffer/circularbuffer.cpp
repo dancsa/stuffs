4212ae97a0f48c806d0fe602a99d3b25b58e82bd
GALAMBOS Daniel <dancsa@dancsa.hu>
2014-12-09 01:02:26 +0100

    circular/ring byte buffer implementation

---
+++ buffer/circularbuffer.cpp
@@ -0,0 +1,294 @@
+/*
+Copyright (c) 2014, GALAMBOS Dániel <dancsa@dancsa.hu>
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#include <cstddef> //size_t
+#include <stdexcept>
+#include <string>
+
+#include <cstring>
+#include "circularbuffer.hpp"
+
+using namespace dancsa;
+using std::size_t;
+
+/* _const_circular_buffer_itr impementation below */
+_const_circular_buffer_itr & 
+_const_circular_buffer_itr::operator=(const _const_circular_buffer_itr & o){
+	this->buffer = o.buffer; 
+	this->idx = o.idx;
+}
+
+bool 
+_const_circular_buffer_itr::operator==(const _const_circular_buffer_itr & o){
+	return ( this->buffer == o.buffer && this->idx == o.idx);
+}
+
+bool 
+_const_circular_buffer_itr::operator!=(const _const_circular_buffer_itr & o){
+	return !(*this == o);
+}
+
+_const_circular_buffer_itr & 
+_const_circular_buffer_itr::operator++(){
+	this->idx++;
+	return *this;
+}
+
+_const_circular_buffer_itr 
+_const_circular_buffer_itr::operator++(int foo){
+	_const_circular_buffer_itr tmp(*this);
+	this->operator++();
+	return tmp;
+}
+
+const char & 
+_const_circular_buffer_itr::operator*(){
+	return (*(this->buffer))[idx];
+}
+
+
+
+/* CircularBuffer impementation below */
+
+CircularBuffer::CircularBuffer( const CircularBuffer& o){
+	this->currsize = o.currsize;
+	this->capacity = o.capacity;
+	this->start = o.start;
+	this->buffer = new char[o.capacity];
+	std::memcpy(this->buffer, o.buffer, this->capacity);
+}
+
+
+/**
+	@return bytes available for reading from puffer
+*/
+size_t 
+CircularBuffer::size() const{
+	return this->currsize;
+}
+
+
+/**
+	Empties the puffer.
+*/
+void 
+CircularBuffer::clear(){
+	this->currsize = 0;
+}
+
+
+size_t 
+CircularBuffer::lastidx() const { 
+	if (0 == currsize){ 
+		return start;
+	}
+	else{
+		return ((start+currsize-1) % capacity);
+	}
+}
+
+
+/**
+	@returns constant iterator to the first (oldest) byte
+*/
+CircularBuffer::const_iterator 
+CircularBuffer::begin() const{
+	return const_iterator(this, 0);
+}
+
+
+/**
+	@returns constant iterator to after the lastest byte.
+*/
+CircularBuffer::const_iterator 
+CircularBuffer::end() const {
+	return const_iterator(this, this->size());	
+}
+
+
+/**
+	@return free bytes in the buffer
+*/
+size_t 
+CircularBuffer::availsize() const{
+	return ( this->capacity - this->currsize);
+}
+
+
+/**
+	@returns true if and only if there are no data in the puffer
+*/
+bool 
+CircularBuffer::empty() const{
+	return ( 0 == this->currsize);
+}
+
+
+/**
+	@param idx index
+	@returns reference to the indexth element's reference to modify the puffer
+	This should be used for testing/debugging reasons only.
+*/
+char & 
+CircularBuffer::operator[](std::size_t idx){
+	if( idx >= this->currsize){
+		throw new std::out_of_range("overindexing");
+	}
+	return buffer[ (this->start + idx) % this->capacity ];
+}
+
+
+/**
+	@param idx index
+	@returns constant reference to the indexth element of the puffer (peeking in the puffer)
+*/
+const char & 
+CircularBuffer::operator[](std::size_t idx) const {
+	if( idx >= this->currsize){
+		throw new std::out_of_range("overindexing");
+	}
+	return buffer[ (this->start + idx) % this->capacity ];
+}
+
+
+/**
+	@param input to overwrite the buffer with.
+	Do NOT use this, if your string contains null byte, use clear() and add()
+*/
+CircularBuffer & 
+CircularBuffer::operator=(const std::string &input){
+	return this->operator=(input.c_str());
+}
+
+
+/**
+	@param input c-string to overwrite the  puffer with.
+	Note that the terminating null byte is NOT included.
+*/
+CircularBuffer & 
+CircularBuffer::operator=(const char * input){
+	size_t len = std::strlen(input);
+	if( len > this->capacity ){
+		throw new std::out_of_range("buffer can't handle this much data");
+	}
+	this->start = 0;
+	this->currsize = len;
+	std::memcpy(this->buffer, input, len);
+	return *this;	
+}
+
+
+/**
+	@param input append to puffer.
+	@throws std::out_of_range if there are no enough space
+	Do NOT use this, if your string contains null byte, use add()
+*/
+CircularBuffer & 
+CircularBuffer::operator+(const std::string &input){
+	return this->operator+(input.c_str());
+}
+
+
+/**
+	@param input c-string, append to buffer
+	@throws std::out_of_range if there are no enough space
+	Note that the terminating null byte is NOT included.
+*/
+CircularBuffer & 
+CircularBuffer::operator+(const char * input){
+	size_t len = std::strlen(input);
+	this->add(input, len);
+	return *this;
+}
+
+
+/**
+	@param buff address of data to be appended
+	@param len length of data
+	@throws std::out_of_range if there are no enough space
+	Caller must ensure that there are enough free space in the buffer.
+	Zero byte append is allowed.
+*/
+void
+CircularBuffer::add(const void * buff, size_t len){
+	if( len > this->availsize() ){
+		throw new std::out_of_range("buffer can't handle or more than availsize data");
+	}
+	if ( 0 == len ){
+		return;
+	}
+	using std::memcpy;
+
+	size_t end = this->lastidx(); //last used byte's index
+	if( end < start ){
+		memcpy(buffer+end+1, buff, len);
+	}else if(0 == currsize){
+		start=0;
+		memcpy(buffer, buff, len);
+	}
+	else{
+		size_t free_at_end = this->capacity - end - 1;
+		if( free_at_end >= len){ //will not roll over
+			memcpy(buffer+end+1, buff, len);
+		}else{
+			memcpy(buffer+end+1, buff, free_at_end);
+			memcpy(buffer, ((char*) buff)+free_at_end, len-free_at_end);
+		}
+	}
+	currsize += len;
+}
+
+
+/**
+	@param buff memory address to copy the data
+	@param len length of the data to be extracted
+	@throws std::out_of_range if more than available data requested
+	Caller must not pop more data than the buffer current size.
+*/
+void 
+CircularBuffer::pop(void * buff, size_t len){
+	using std::memcpy;
+	if ( 0 == len ){ //appending nothing can be valid.
+		return;
+	}
+	if( len > this->currsize ){
+		throw new std::out_of_range("cant get more data than size()");
+	}
+	
+	size_t end = this->lastidx(); //last index
+	if( end >= start ){
+		memcpy(buff, buffer+start, len);
+		start+=len;
+		currsize-=len;
+	}else{
+		memcpy(buff, buffer+start, capacity-start);
+		memcpy(((char*) buff)+(capacity-start), buffer, len-(capacity-start));
+		start = (start+len) % capacity;
+		currsize-=len;
+	}
+}
+
+
+/**
+	Convience function to return data as std::string.
+	@return the requested data as std::string
+	@throws std::out_of_range if more than available data requested
+	Caller must not pop more data than the buffer current size.
+*/
+std::string
+CircularBuffer::pop_as_string(size_t len){
+	char * data = new char[len];
+	this->pop(data, len);
+	std::string ret(data,len);
+	delete[] data;
+	return ret;
+}
+
---
+++ buffer/circularbuffer.hpp
@@ -0,0 +1,89 @@
+/*
+Copyright (c) 2014, GALAMBOS Dániel <dancsa@dancsa.hu>
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#ifndef CIRCULARBUFFER_HPP_INCLUDED
+#define CIRCULARBUFFER_HPP_INCLUDED
+
+#include <cstddef> //size_t
+#include <string>
+
+
+namespace dancsa {
+
+class CircularBuffer;
+
+/**
+	@brief constans input iterator for CircularBuffer class
+*/
+class _const_circular_buffer_itr : public std::iterator<std::input_iterator_tag, char>{
+	private:
+		const CircularBuffer * buffer;
+		std::size_t idx;
+
+	public:
+		_const_circular_buffer_itr( const CircularBuffer * _buffer, size_t _idx) : buffer(_buffer), idx(_idx){};
+		_const_circular_buffer_itr( const _const_circular_buffer_itr &) = default;
+		_const_circular_buffer_itr & operator=(const _const_circular_buffer_itr & o);
+
+		bool operator==(const _const_circular_buffer_itr & o);
+		bool operator!=(const _const_circular_buffer_itr & o);
+		const char &  operator*();
+		_const_circular_buffer_itr & operator++();
+		_const_circular_buffer_itr operator++(int foo);
+};
+
+
+
+/**
+	@brief Circular/ring buffer class for arbitary binary data.
+	Data can be pushed back or poped from it.
+*/
+class CircularBuffer{
+	private:
+		char * buffer;
+		std::size_t capacity;
+		std::size_t start; //offsets from buffer
+		std::size_t currsize;
+		
+		std::size_t lastidx() const; //get the inner index of the last item. used internally
+
+	public:
+		/** typedef to input iterator */
+		typedef _const_circular_buffer_itr const_iterator; 
+
+		/**
+			Constructor
+			@param _size buffer size to be allocated.
+		*/
+		CircularBuffer(std::size_t _size) : capacity(_size),start(0),currsize(0) { buffer = new char[currsize]; };
+		CircularBuffer( const CircularBuffer& o);
+		~CircularBuffer() { delete[] buffer; };
+		std::size_t size() const;
+		std::size_t availsize() const;
+		char & operator[](std::size_t idx);
+		const char & operator[](std::size_t idx) const;
+		CircularBuffer & operator=(const std::string &input);
+		CircularBuffer & operator=(const char * input);
+		CircularBuffer & operator+(const std::string  &input);
+		CircularBuffer & operator+(const char * input);
+		bool empty() const;
+		void clear();
+		void add(const void * buff, std::size_t len); 
+		void pop(void * buff, std::size_t len);
+		std::string pop_as_string(size_t len);
+		const_iterator begin() const;
+		const_iterator end() const;
+
+};
+
+
+} //namespace
+#endif //include guard
